16437317e4eee258dec7bf8fec5ce0c81457cbff
Steven Deobald <steven@deobald.ca>
2025-07-23 08:49:00 +0200

    thunderbird and fractal

--- install-flatpaks.sh
+++ install-flatpaks.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
-# signal
-flatpak install flathub org.signal.Signal
+# thunderbird
+flatpak install flathub org.mozilla.Thunderbird
 
 # apostrophe
 flatpak install flathub org.gnome.gitlab.somas.Apostrophe
 
+# signal
+flatpak install flathub org.signal.Signal
+
 # telegram
 flatpak install flathub org.telegram.desktop -y
+
+# fractal
+flatpak install flathub org.gnome.Fractal
 
 # slack
 flatpak install flathub com.slack.Slack
